9e70fefb7454b155358a53d22f241186fd7609c1
Adam Linkhart <alinkhart@gmail.com>
2022-05-10 11:57:34 -0400

    Initial Commit

---
+++ Shared/NKU_FinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  NKU_FinalApp.swift
+//  Shared
+//
+//  Created by Adam Linkhart on 5/10/22.
+//
+
+import SwiftUI
+
+@main
+struct NKU_FinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
